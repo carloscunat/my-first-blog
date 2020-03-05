06ad7d2b788c4ef0921caa7caf43762b30d7380d
Carlos Cuñat <carloscunyat29@gmail.com>
2020-03-05 13:18:54 +0100

    intento de arreglar fallos details 1

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'', views.post_list, name='post_list'),
-    url('post/<int:pk>/', views.post_detail, name='post_detail'),
+    url(r'post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
