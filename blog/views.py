8b799b8efbadee99e47f8650add02fbff0df0959
Carlos Cuñat <carloscunyat29@gmail.com>
2020-03-05 12:30:15 +0100

    Cosas

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Post
 from django.shortcuts import render
-
+from django.utils import timezone
 # Create your views here.
 def post_list(request):
+    Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {})
