c7d79541ff9728006e4c00b0c5234221f23f7b2c
Carlos Cuñat <carloscunyat29@gmail.com>
2020-03-05 13:05:23 +0100

    nuevo detail

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from django.utils import timezone
 # Create your views here.
 def post_list(request):
     Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
     return render(request, 'blog/post_list.html', {})
+def post_detail(request, pk):
+    
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
